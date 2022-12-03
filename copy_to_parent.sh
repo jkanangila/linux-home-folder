fc8e13831bfe8aed9f4751d4d273766967a53bad
jkanangila <jkanangila@gmail.com>
2022-12-03 19:51:03 +0100

    Fixed copy script and modified vim options

--- copy_to_parent.sh
+++ copy_to_parent.sh
@@ -1,18 +1,20 @@
 #!/bin/sh
 
 # FILES
-NVIM="$HOME"/.config/nvim
-ZSH="$HOME"/.config/zsh
-SCRIPTS="$HOME"/scripts
+CONFIG="$HOME"/.config
 IMAGE="$HOME"/nvim-appimage
 
 cd $HOME/linux-home-folder
 mkdir -p $HOME/.config
-mkdir -p $NVIM && yes | cp -r .config/nvim $NVIM
+
+yes | cp -r .config/nvim $CONFIG
 echo "copied vim config"
-mkdir -p $ZSH && yes | cp -r .config/zsh $ZSH
+
+yes | cp -r .config/zsh $CONFIG
 echo "copied zsh config"
-mkdir -p $SCRIPTS && yes | cp -r scripts $SCRIPTS
+
+yes | cp -r scripts $HOME
 echo "copied scripts"
+
 mkdir -p $IMAGE && yes | cp nvim.appimage $IMAGE
 echo "copied nvim.appimage"
