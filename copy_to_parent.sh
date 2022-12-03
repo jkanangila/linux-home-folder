5f1f96eacf27f9261d99a0683aed7b89bb1b8fdd
jkanangila <jkanangila@gmail.com>
2022-12-03 19:02:32 +0100

    Made scrit executable

--- copy_to_parent.sh
+++ copy_to_parent.sh
@@ -6,8 +6,13 @@ ZSH="$HOME"/.config/zsh
 SCRIPTS="$HOME"/scripts
 IMAGE="$HOME"/nvim-appimage
 
+cd $HOME/linux-home-folder
 mkdir -p $HOME/.config
 mkdir -p $NVIM && yes | cp -r .config/nvim $NVIM
+echo "copied vim config"
 mkdir -p $ZSH && yes | cp -r .config/zsh $ZSH
+echo "copied zsh config"
 mkdir -p $SCRIPTS && yes | cp -r scripts $SCRIPTS
+echo "copied scripts"
 mkdir -p $IMAGE && yes | cp nvim.appimage $IMAGE
+echo "copied nvim.appimage"
