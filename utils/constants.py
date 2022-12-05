6c7a404f0c4ac568703bc0ed3b3a64889296583b
jkanangila <jkanangila@gmail.com>
2022-12-05 22:03:46 +0100

    progress from android

--- utils/constants.py
+++ utils/constants.py
@@ -1,4 +1,11 @@
 from pathlib import Path
+from enum import Enum
 
 
 HOME = Path.home()
+
+class PlatformOptions(Enum):
+    LINUX='linux'
+    WINDOWS='win32'
+    CYGWIN='cygwin'
+    OSX='darwin'
--- utils/shell.py
+++ utils/shell.py
@@ -1,9 +1,19 @@
-def get_distro_name():
-    pass
+from shutil import which
+import subprocess
+import sys
+from utils.constants import PlatformOptions
+
+def get_distro_name() ->:
+    if hasattr(sys, "getandroidapilevel"):
+        return "android"
+    if sys.platform == PlatformOptions.LINUX.value:
+        pass
 
 def get_install_directive() -> str:
     pass
 
-def execute_command():
-    pass
+def execute_command(cmd: str):
+    subprocess.run(cmd.split(" "), check=True)
 
+def is_installed(name: str) -> bool:
+    return which(name) is not None
