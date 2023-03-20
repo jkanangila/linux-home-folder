a5f98e060d7da5e1b06e243ee0334a581447d375
jkanangila <jkanangila@gmail.com>
2023-03-20 16:05:25 +0100

    edit pyenv install script (progress)

---
+++ scripts/sh/install-pyenv.sh
@@ -0,0 +1,5 @@
+# 1. DEBIAN: Install the dependencies necessary to build Python:
+# TODO Place inside a conditional to verify the package manager
+sudo apt install build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libsqlite3-dev libreadline-dev libffi-dev curl libbz2-dev
+
+# 2. INSTALL Pyenv
