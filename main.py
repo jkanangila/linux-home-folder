ab702f5ac677e5b112443b32db4f92eabc30cae8
John Kanangila Mulamba <jkanangila@gmail.com>
2026-06-16 22:43:43 +0100

    clean: remove obsolete files

--- main.py
+++
@@ -1,8 +0,0 @@
-from utils.set_parser import config_parser
-from scripts import resolver
-
-if __name__ == "__main__":
-    parser = config_parser()
-    args = parser.parse_args()
-
-    resolver(args)
--- src/.zshrc
+++
@@ -1,5 +0,0 @@
-export ZDOTDIR=$HOME/.config/zsh
-export SPACESHIP_CONFIG="$ZDOTDIR/spaceship.zsh"
-#export BROWSER=wslview # uncomment on wsl
-
-source $ZDOTDIR/.zshrc
