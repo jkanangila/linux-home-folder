136784c1474d04575fa3cfcb30daab7d571b6e42
John Mulamba Kanangila <jkanangila@gmail.com>
2026-07-08 16:19:55 +0100

    refactor(config): remap comment-box keys and update zsh PATH
    - Change comment-box title shortcut from <leader>bb to <leader>bt
    - Add Antigravity CLI installer path to .zshrc

--- nvim/.config/nvim/lua/plugins/comment-box.lua
+++ nvim/.config/nvim/lua/plugins/comment-box.lua
@@ -4,12 +4,12 @@ return {
   -- Trigger the plugin to load only when you invoke these keys
   keys = {
     -- Normal Mode
-    { "<leader>bb", "<cmd>CBccbox<cr>", mode = "n", desc = "Box Title (Centered)" },
+    { "<leader>bt", "<cmd>CBccbox<cr>", mode = "n", desc = "Box Title (Centered)" },
     { "<leader>bl", "<cmd>CBllbox<cr>", mode = "n", desc = "Box Title (Left Aligned)" },
     { "<leader>bd", "<cmd>CBd<cr>", mode = "n", desc = "Remove Box" },
 
     -- Visual Mode (Highlight text blocks first)
-    { "<leader>bb", "<cmd>CBccbox<cr>", mode = "v", desc = "Box Selection (Centered)" },
+    { "<leader>bt", "<cmd>CBccbox<cr>", mode = "v", desc = "Box Selection (Centered)" },
     { "<leader>bl", "<cmd>CBllbox<cr>", mode = "v", desc = "Box Selection (Left Aligned)" },
   },
 }
--- zsh/.zshrc
+++ zsh/.zshrc
@@ -3,3 +3,7 @@ export SPACESHIP_CONFIG="$ZDOTDIR/spaceship.zsh"
 #export BROWSER=wslview # uncomment on wsl
 
 source $ZDOTDIR/.zshrc
+
+
+# Added by Antigravity CLI installer
+export PATH="/home/jkanangila/.local/bin:$PATH"
